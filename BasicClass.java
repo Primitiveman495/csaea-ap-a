5bf52b86d4ff00d0c643fe8b9ff3bfc01fbed832
Primitiveman495 <158346475+Primitiveman495@users.noreply.github.com>
2026-09-10 13:08:11 +0000

    verrrrrry basic

---
+++ BasicClass.java
@@ -0,0 +1,15 @@
+public class BasicClass {
+    public static void main(String[] args) {
+
+        /*
+        Attributes/data 
+        Variables
+
+        Constructor --- SPECIAL METHOD called every time you make an object, same name as the class followed by ()
+        EX: BasicClass()
+
+        Behiaviors/methods
+        */
+
+    }
+}
